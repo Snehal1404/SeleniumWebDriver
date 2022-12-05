8ee875f485ad6d1c31be8331dee082282094848d
Snehal1404 <snehal21songire@gmail.com>
2022-12-05 12:53:01 +0530

    comments

---
+++ Payback.in/Utility/ConfigClass.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Payback_Po.Utility
+{
+    public class ConfigClass
+    {
+        public static IWebDriver? driver;
+        public static string chromeDriverPath = @"D:\CAPGEMINI\C# Practice\Payback.in\Payback.in\Payback.in\Driver\chromedriver_win32\";
+       // public static string geckoDriverPath = @"C:\Users\username\source\repos\SeleniumSpecFlowPO\SeleniumSpecFlowPO\drivers\";
+       
+        public static string url = "https://www.payback.in/";
+
+        public static string filePath = @"D:\CAPGEMINI\C# Practice\Payback.in\Payback.in\Payback.in\ScreenShot\";
+
+        public static int PAGE_LOAD_TIMEOUT = 10;
+        public static int IMPLICIT_WAIT = 10;
+
+        public static Actions? action;
+    }
+}
